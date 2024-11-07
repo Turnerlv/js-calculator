9d81b66ae019ee1dea81a33d97169e98a3f14318
“turnerlv” <turner.vickery@gmail.com>
2024-11-07 01:03:54 -0600

    adds html structure & css styles

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Cached elements
+const calcButtons = document.querySelector('.buttons')
+
+
+/*-----------Model-----------*/
+
+//State variables
+
+
+/*----------controller--------*/
+
+
+
+/*-------------View------------*/
